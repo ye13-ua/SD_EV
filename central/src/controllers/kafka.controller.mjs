d333f2b9b45a3d28f2e7d93d686a56f1cd1f7137
Artur Ramazanov Anichkin <ara139@alu.ua.es>
2025-10-25 20:21:01 +0200

    Se empezaron a crear los productores de kafka en central

---
+++ central/src/controllers/kafka.controller.mjs
@@ -0,0 +1,8 @@
+import { Kafka } from "kafkajs";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const kafka = new Kafka({
+    
+})
--- central/src/db/connection.mjs
+++ central/src/db/connection.mjs
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const {DB_HOST, DB_NAME, DB_USER, DB_PASSWORD} = process.env
 
 //CONEXION A LA BASE DE DATOS
-export const sequelize = new Sequelize ("evcharging","usuario","contraseña", {
-    host: "db",
+export const sequelize = new Sequelize (DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
     dialect: "postgres",
     logging: false
 })
