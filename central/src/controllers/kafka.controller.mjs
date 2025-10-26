efe55048cf642175ea3cb56d8a2e013941a9546e
Artur Ramazanov Anichkin <ara139@alu.ua.es>
2025-10-26 09:56:13 +0100

    Terminar kafka producer

--- central/src/controllers/kafka.controller.mjs
+++ central/src/controllers/kafka.controller.mjs
@@ -4,5 +4,26 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const kafka = new Kafka({
-    
+    clientId: "EV_Central",
+    brokers: [process.env.KAFKA_BOOTSTRAP]
 })
+
+export async function produceMessage(message) {
+    const producer = kafka.producer();
+    await producer.connect();
+
+    await producer.send({
+        //topic: ,
+        messages: [{ value: message}]
+    })
+
+    console.log("Enviando mensaje", message);
+    await producer.disconnect()
+}
+
+export async function consumeMessage() {
+    const consumer = kafka.consumer({ groupId: "central_cmd"})
+}
+
+
+
